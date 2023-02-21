01d7e2d8c77ab0c2a9ee053fba046bc52e7081d5
Timicasto Index <timicasto@gmail.com>
2023-02-22 03:30:00 +0800

    app complete reduced decoder for IFU except several ToDos

--- src/main/scala/core/pipeline/IFReducedDecoder.scala
+++ src/main/scala/core/pipeline/IFReducedDecoder.scala
@@ -27,7 +27,7 @@ class IFReducedDecoder extends Module {
 		val valRS1: UInt = Output(UInt(5.W))
 		val valRS2: UInt = Output(UInt(5.W))
 		val valJalrRS1: UInt = Output(UInt(5.W))
-		val ValBjpImm: UInt = Output(UInt(32.W))
+		val valBjpImm: UInt = Output(UInt(32.W))
 	})
 	
 	val isLongIns: Bool = (((~(io.inInstruction.apply(4, 2) === 7.U)).asUInt) & ((io.inInstruction.apply(1, 0) === 3.U).asUInt)).asBool
@@ -46,6 +46,36 @@ class IFReducedDecoder extends Module {
 	private val isCompressedJalr: Bool = (isCompressedJalrMask & (io.inInstruction.apply(12, 12)).asUInt & (~(io.inInstruction.apply(11, 7) === 0.U)).asUInt & (io.inInstruction.apply(6, 2) === 0.U)).asBool
 	
 	io.isJalr := isLongJalr | isCompressedJalr | isCompressedJr
+	
+	private val isLongBranch: Bool = ((io.inInstruction.apply(6, 5) === 3.U).asUInt & (io.inInstruction.apply(4, 2) === 0.U).asUInt & (io.inInstruction.apply(1, 0) === 3.U).asUInt).asBool
+	private val isCompressedBeqz: Bool = ((io.inInstruction.apply(1, 0) === 1.U).asUInt & (io.inInstruction.apply(15, 13) === 6.U).asUInt).asBool
+	private val isCompressedBnez: Bool = ((io.inInstruction.apply(1, 0) === 1.U).asUInt & (io.inInstruction.apply(15, 13) === 7.U).asUInt).asBool
+	
+	io.isConditionedJmp := isLongBranch | isCompressedBeqz | isCompressedBnez
+	
+	private val isLongOp: Bool = ((io.inInstruction.apply(6, 5) === 1.U).asUInt & (io.inInstruction.apply(4, 2) === 4.U).asUInt & (io.inInstruction.apply(1, 0) === 3.U)).asBool
+	
+	// rv32 func3 14:12 rv32 func7 31:25 rv32 rd 11:7 rv32 rs1 rs2 19:15 24:20
+	private val isLongMulh: Bool = ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 1.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt).asBool
+	private val isLongMulhsu: Bool = ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 2.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt).asBool
+	private val isLongMulhu: Bool = ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 3.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt).asBool
+	
+	io.isMulhsu := isLongMulh | isLongMulhu | isLongMulhsu
+	
+	io.isMul := ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 0.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt)
+	io.isDiv := ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 4.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt)
+	io.isDivU := ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 5.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt)
+	io.isRem := ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 6.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt)
+	io.isRemU := ((isLongOp.asUInt) & (io.inInstruction.apply(14, 12) === 7.U).asUInt & (io.inInstruction.apply(31, 25) === 1.U).asUInt)
+	
+	// TODO: Compressed instruction RS value decoding
+	io.valRS1 := (io.inInstruction.apply(19, 15))
+	io.valRS2 := (io.inInstruction.apply(24, 20))
+	
+	// TODO: RS Enable signal decoding
+	
+	io.valJalrRS1 := Mux(isLongIns, io.inInstruction.apply(19, 15), io.inInstruction.apply(11, 7))
+	// TODO: BJP instruction immediate number decoding
 	
 	io.isJmp := io.isJal | io.isJalr | io.isConditionedJmp
 }
